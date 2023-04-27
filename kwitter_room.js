7843f36df0790a0cf19e4987ad89213e706b7cb8
ClaraLasmar <114614612+ClaraLasmar@users.noreply.github.com>
2023-04-27 09:19:26 -0400

    Create kwitter_room.js

---
+++ kwitter_room.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCdpk5kXfdcOObjtGHCelHIPv-pxW599-g",
+  authDomain: "kwitter-4e928.firebaseapp.com",
+  databaseURL: "https://kwitter-4e928-default-rtdb.firebaseio.com",
+  projectId: "kwitter-4e928",
+  storageBucket: "kwitter-4e928.appspot.com",
+  messagingSenderId: "631436909810",
+  appId: "1:631436909810:web:cff5b0bd2662da5ef3c46b"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
